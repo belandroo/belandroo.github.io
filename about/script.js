a411d793b0ae8ddb264b738edf738a91fccf5ae3
Andrew Lewis <belandroo@suse.attlocal.net>
2026-04-18 22:07:50 -0400

    Initial portfolio upload

---
+++ about/script.js
@@ -0,0 +1,126 @@
+const input = document.getElementById("input");
+const output = document.getElementById("output");
+const prompt = "user@portfolio:~>"
+const fileSystem = {
+  home: {},
+  projects: {},
+  about: {},
+  contact: {},
+  
+  "readme.md": `
+- This is a clever simulated terminal that allows the user to navigate between pages on my website.
+- There are different files to check out within each page.
+  `,
+
+  "skills.txt": `
+Linux
+Bash
+Networking
+Virtualization
+  `,
+}
+
+let commandHistory = [];
+let historyIndex = -1;
+
+function print(text) {
+  output.innerHTML += `<p>${text}</p>`;
+  output.scrollTop = output.scrollHeight;
+}
+
+input.addEventListener("keydown", function(e) {
+
+  // ENTER = run command
+  if (e.key === "Enter") {
+    const command = input.value.trim();
+
+    print(`> ${command}`);
+    commandHistory.push(command);
+    historyIndex = commandHistory.length;
+
+    handleCommand(command);
+
+    input.value = "";
+  }
+
+  // UP ARROW = previous command
+  if (e.key === "ArrowUp") {
+    if (historyIndex > 0) {
+      historyIndex--;
+      input.value = commandHistory[historyIndex];
+    }
+  }
+
+  // DOWN ARROW = next command
+  if (e.key === "ArrowDown") {
+    if (historyIndex < commandHistory.length - 1) {
+      historyIndex++;
+      input.value = commandHistory[historyIndex];
+    } else {
+      input.value = "";
+    }
+  }
+
+});
+
+function handleCommand(command) {
+
+  if (command === "help") {
+    print("Available commands:");
+    print("help, ls, pwd, cat, cd, clear, whoami");
+  }
+
+  else if (command === "whoami") {
+    print("Andrew Lewis, an aspiring Linux Admin");
+  }
+
+  else if (command === "ls") {
+    const fileList = Object.keys(fileSystem).join("  ");
+    print(fileList);
+    
+  }
+
+  else if (command === "pwd") {
+    print("about")
+  }
+
+  else if (command.startsWith("cat ")) {
+    const fileName = command.split(" ")[1];
+
+    if (fileSystem[fileName]) {
+      print(fileSystem[fileName]);
+    } else {
+      print(`cat: ${fileName}: No such file`);
+    }
+  }
+
+  else if (command.startsWith("cd ")) {
+    const target = command.split(" ")[1];
+
+    if (target === "home") {
+      window.location.href = "../homepage/index.html";
+    }
+    else if (target === "projects") {
+      window.location.href = "../projects/index.html";
+    }
+    else if (target === "about") {
+      window.location.href = "./index.html";
+    }
+    else if (target === "contact") {
+      window.location.href = "./contact/";
+    }
+    else {
+      print(`cd: no such directory: ${target}`);
+    }
+  }
+
+  else if (command === "clear") {
+    output.innerHTML = "";
+    print("Welcome to my portfolio");
+    print("Type 'help' to see available commands");
+  }
+
+  else {
+    print(`command not found: ${command}`);
+  }
+}
---
+++ projects/script.js
@@ -0,0 +1,127 @@
+const input = document.getElementById("input");
+const output = document.getElementById("output");
+const prompt = "user@portfolio:~>"
+const fileSystem = {
+  home: {},
+  projects: {},
+  about: {},
+  contact: {},
+  
+  "project1.txt": `
+Linux
+Bash
+Networking
+Virtualization
+  `,
+
+ /* "projects.txt": `
+1. Home Lab Server
+2. Portfolio Website
+3. Network Simulation Lab
+  ` */
+}
+
+let commandHistory = [];
+let historyIndex = -1;
+
+function print(text) {
+  output.innerHTML += `<p>${text}</p>`;
+  output.scrollTop = output.scrollHeight;
+}
+
+input.addEventListener("keydown", function(e) {
+
+  // ENTER = run command
+  if (e.key === "Enter") {
+    const command = input.value.trim();
+
+    print(`> ${command}`);
+    commandHistory.push(command);
+    historyIndex = commandHistory.length;
+
+    handleCommand(command);
+
+    input.value = "";
+  }
+
+  // UP ARROW = previous command
+  if (e.key === "ArrowUp") {
+    if (historyIndex > 0) {
+      historyIndex--;
+      input.value = commandHistory[historyIndex];
+    }
+  }
+
+  // DOWN ARROW = next command
+  if (e.key === "ArrowDown") {
+    if (historyIndex < commandHistory.length - 1) {
+      historyIndex++;
+      input.value = commandHistory[historyIndex];
+    } else {
+      input.value = "";
+    }
+  }
+
+});
+
+function handleCommand(command) {
+
+  if (command === "help") {
+    print("Available commands:");
+    print("help, ls, pwd, cat, cd, clear, whoami");
+  }
+
+  else if (command === "whoami") {
+    print("Andrew Lewis, an aspiring Linux Admin");
+  }
+
+  else if (command === "ls") {
+    const fileList = Object.keys(fileSystem).join("  ");
+    print(fileList);
+    
+  }
+
+  else if (command === "pwd") {
+    print("about")
+  }
+
+  else if (command.startsWith("cat ")) {
+    const fileName = command.split(" ")[1];
+
+    if (fileSystem[fileName]) {
+      print(fileSystem[fileName]);
+    } else {
+      print(`cat: ${fileName}: No such file`);
+    }
+  }
+
+  else if (command.startsWith("cd ")) {
+    const target = command.split(" ")[1];
+
+    if (target === "home") {
+      window.location.href = "../homepage/index.html";
+    }
+    else if (target === "projects") {
+      window.location.href = "./index.html";
+    }
+    else if (target === "about") {
+      window.location.href = "../about/index.html";
+    }
+    else if (target === "contact") {
+      window.location.href = "./contact/";
+    }
+    else {
+      print(`cd: no such directory: ${target}`);
+    }
+  }
+
+  else if (command === "clear") {
+    output.innerHTML = "";
+    print("Welcome to my portfolio");
+    print("Type 'help' to see available commands");
+  }
+
+  else {
+    print(`command not found: ${command}`);
+  }
+}
